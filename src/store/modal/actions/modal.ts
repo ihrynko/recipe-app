7dc10696406244aa35add6b072bad393dce03c1b
Iryna Hrynko <ir.hrynko@gmail.com>
2022-08-31 11:16:12 +0200

    Update modal.ts

--- src/store/modal/actions/modal.ts
+++ src/store/modal/actions/modal.ts
@@ -1,19 +1,17 @@
-import { PayloadAction } from '@reduxjs/toolkit';
-import { SliceState } from '../reducers/modal';
+import { PayloadAction } from "@reduxjs/toolkit";
+import { SliceState } from "../reducers/modal";
 
 export enum MODAL_NAME {
- CATEGORY_CREATE = 'CATEGORY_CREATE',
- CATEGORY_DELETE = 'CATEGORY_DELETE',
- RECIPE_CREATE = 'RECIPE_CREATE',
-RECIPE_DELETE = 'RECIPE_DELETE',
-RECIPE_INGREDIENTS = 'RECIPE_INGREDIENTS',
- RECIPE_METHOD = 'RECIPE_METHOD',
+  CATEGORY_CREATE = "CATEGORY_CREATE",
+  CATEGORY_DELETE = "CATEGORY_DELETE",
+  RECIPE_CREATE = "RECIPE_CREATE",
+  RECIPE_DELETE = "RECIPE_DELETE",
 }
 
 export const modalOpenToggle = (
   state: SliceState,
   action: PayloadAction<{ name: MODAL_NAME }>
 ) => {
-   state.open = !state.open;
+  state.open = !state.open;
   state.name = action.payload?.name;
 };
