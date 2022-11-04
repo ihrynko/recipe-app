aeff4f673044717fe88d7afeb7eda0c17876e8aa
Iryna Hrynko <ir.hrynko@gmail.com>
2022-11-04 10:43:30 +0100

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,10 +1,27 @@
 import axios from "axios";
+import type { AxiosRequestConfig } from "axios";
 
-export const client= axios.create({
+export const client = axios.create({
   baseURL: "http://localhost:80/api/v1/",
 });
 
 client.interceptors.response.use(
   (response) => response.data.data,
-  (error) => Promise.reject(error)
+  (error) => {
+    if (error.message === "Request failed with status code 401") {
+      localStorage.clear();
+    }
+    return Promise.reject(error);
+  }
+);
+
+client.interceptors.request.use(
+  (config: AxiosRequestConfig) => {
+    config.headers = config.headers ?? {};
+    config.headers.authorization = `Bearer ${localStorage.getItem("token")}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
